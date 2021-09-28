4fb88265d46055e535151e893b9a61790f9cc15f
Bahin Mathieu <33661850+MathieuBahin@users.noreply.github.com>
2021-09-28 13:57:42 +0200

    Communication, rechargement depuis EEPROM, mode pilote
    vérifier les define des set_Pilote

--- GSM_Controleur_Radiateur/GSM_Controleur_Radiateur.ino
+++ GSM_Controleur_Radiateur/GSM_Controleur_Radiateur.ino
@@ -1,158 +1,332 @@
-#define Pin_Pilote_High 13 /* D13 */
-#define Pin_Pilote_Low  12 /* D12 */
-#define Pin_Temperature 4  /* D4  */
+#define Pin_Pilote_High 13 /*D13 */ 
+#define Pin_Pilote_Low 12 /*D12 */ 
+#define Pin_Temperature 4 /*D4  */
 
-#include "OneWire.h"
-#include "SoftwareSerial.h"
+#include "OneWire.h"	// capteur température
+#include "SoftwareSerial.h"	// SIM800L
+#include <EEPROM.h>
+#include <avr/wdt.h >	// watchdog pour reset
 
-SoftwareSerial Serial_Sim(6,5); // pin6 =  D6 = RX, pin5 =  D5 = TX
-OneWire  Temperature(Pin_Temperature);  
-  
-byte type_s;
-byte data[12];
-byte addr[8];
-byte present = 0;
-float celsius;
-char sms_text[128];
-int sms_text_index = 0;
+SoftwareSerial Serial_Sim(6, 5);	// pin6 =  D6 = RX, pin5 =  D5 = TX  Arduino Nano
+OneWire Temperature(Pin_Temperature);
 
-void setup() {
-  // put your setup code here, to run once:
-  pinMode(Pin_Pilote_High, OUTPUT);
-  pinMode(Pin_Pilote_Low, OUTPUT);
-  //delay(7000);
-  Serial.begin(9600);
-  Serial_Sim.begin(9600);
-  delay(1000);
-  Serial_Sim.println("AT");
-  updateSerial();
-  Serial.println("Initialisation terminée");
-  updateSerial();
+// structure contenant les infos liées au capteur de température  
+struct st_Data_Temp
+{
+	byte type_s;
+	byte data[12];
+	byte addr[8];
+	byte present;
+	float celsius;
+};
+st_Data_Temp data_temp;
 
-  Serial_Sim.println("AT+CMGF=1"); // configure TEXT mode
-  updateSerial();
-  Serial_Sim.println("AT+CNMI=1,2,0,0,0");
-  updateSerial();
+#define CONFORT 1
+#define ECO 2
+#define HORS_GEL 3
+#define ARRET 4
+// structure contenant les infos liées au module SIM800L  
+struct st_Data_Sim
+{
+	char received;	// caractère reçu
+	String sms_data;	// concaténation des caractères reçus
+	String phone_number;	// numéro de téléphone (figé pour le moment)
+	int mode;	// mode du fil pilote (confort, etc)
+};
+st_Data_Sim data_sim;
 
-  if ( !Temperature.search(addr)) {
-    Serial.println("No more addresses.");
-    Serial.println();
-    Temperature.reset_search();
-    delay(250);
-    return;
-  }
-  Serial.print("ROM =");
-  for(int i = 0; i < 8; i++) {
-    Serial.write(' ');
-    Serial.print(addr[i], HEX);
-  }
+int EE_addr = 0;	// adresse EEPROM mode du fil pilote  
 
-  if (OneWire::crc8(addr, 7) != addr[7]) {
-      Serial.println("CRC is not valid!");
-      return;
-  }
-  Serial.println();
- 
-  // the first ROM byte indicates which chip
-  switch (addr[0]) {
-    case 0x10:
-      Serial.println("  Chip = DS18S20");  // or old DS1820
-      type_s = 1;
-      break;
-    case 0x28:
-      Serial.println("  Chip = DS18B20");
-      type_s = 0;
-      break;
-    case 0x22:
-      Serial.println("  Chip = DS1822");
-      type_s = 0;
-      break;
-    default:
-      Serial.println("Device is not a DS18x20 family device.");
-      return;
-  } 
+void setup()
+{
+	pinMode(Pin_Pilote_High, OUTPUT);	// controle MOC haut 
+	pinMode(Pin_Pilote_Low, OUTPUT);	// controle MOC bas
 
-  Temperature.reset();
-  Temperature.select(addr);
-  Temperature.write(0x44, 1);        // start conversion, with parasite power on at the end
-  
-  delay(1000);     // maybe 750ms is enough, maybe not
-  // we might do a ds.depower() here, but the reset will take care of it.
-  present = Temperature.reset();
+	data_sim.phone_number = "+33xxxxxxxxx";	// numéro de téléphone 
 
+	Serial.begin(9600);
+	Serial_Sim.begin(9600);
+
+	delay(1000);
+	Serial_Sim.println("AT");	// répond OK 
+	updateSerial();
+
+	Serial_Sim.println("AT+CMGF=1");	// configure mode TEXT
+	updateSerial();
+	Serial_Sim.println("AT+CNMI=1,2,0,0,0");	// paramètrage réception sms
+	updateSerial();
+
+	Serial.println("GSM OK");
+	updateSerial();
+
+	if (!Temperature.search(data_temp.addr))
+	{
+		Serial.println("Plus d'adresses.");
+		Serial.println();
+		Temperature.reset_search();
+		delay(250);
+		return;
+	}
+
+	if (OneWire::crc8(data_temp.addr, 7) != data_temp.addr[7])
+	{
+		Serial.println("CRC invalide!");
+		return;
+	}
+
+	Serial.println();
+
+	// octet 1 : version du composant 
+	switch (data_temp.addr[0])
+	{
+		case 0x10:
+			//Serial.println("  Chip = DS18S20"); 	// or old DS1820
+			data_temp.type_s = 1;
+			break;
+		case 0x28:
+			//Serial.println("  Chip = DS18B20");
+			data_temp.type_s = 0;
+			break;
+		case 0x22:
+			//Serial.println("  Chip = DS1822");
+			data_temp.type_s = 0;
+			break;
+		default:
+			Serial.println("Composant non reconnu : pas un DS18x20.");
+			return;
+	}
+
+	Temperature.reset();
+	Temperature.select(data_temp.addr);
+	Temperature.write(0x44, 1);	// début conversion, parasite power on 
+	delay(1000);
+	data_temp.present = Temperature.reset();
+
+	Serial.println("Temperature OK");
+
+	data_sim.mode = EEPROM.read(EE_addr);	// recharge le mode du fil pilote depuis l'eeprom
+
+	Serial.println("Initialisation terminée");
+	updateSerial();
+}
+#define set_Confort() set_Pilote(0, 0)
+#define set_Eco() set_Pilote(1, 1)
+#define set_HorsGel() set_Pilote(0, 1)
+#define set_Arret() set_Pilote(1, 0)
+
+/*
+ *positionne les 2 sorties qui controlent les optocoupleurs
+ */
+void set_Pilote(char h, char l)
+{
+	digitalWrite(Pin_Pilote_High, h);
+	digitalWrite(Pin_Pilote_Low, l);
 }
 
-void read_Celsius(){
-  int i;
-  Temperature.reset();
-  Temperature.select(addr);
-  Temperature.write(0x44, 1);        // start conversion, with parasite power on at the end
-  
-  delay(1000);     // maybe 750ms is enough, maybe not
-  // we might do a ds.depower() here, but the reset will take care of it.
-  
-  present = Temperature.reset();
-  Temperature.select(addr);    
-  Temperature.write(0xBE);         // Read Scratchpad
+/*
+ *acquisition et ecriture de la température dans la variable situé dans la structure
+ */
+void read_Celsius()
+{
+	int i;
+	Temperature.reset();
+	Temperature.select(data_temp.addr);
+	Temperature.write(0x44, 1);	// début conversion, parasite power on 
+	delay(1000);
 
-  Serial.print("  Data = ");
-  Serial.print(present, HEX);
-  Serial.print(" ");
-  for ( i = 0; i < 9; i++) {           // we need 9 bytes
-    data[i] = Temperature.read();
-    Serial.print(data[i], HEX);
-    Serial.print(" ");
-  }
-  Serial.print(" CRC=");
-  Serial.print(OneWire::crc8(data, 8), HEX);
-  Serial.println();
+	data_temp.present = Temperature.reset();
+	Temperature.select(data_temp.addr);
+	Temperature.write(0xBE);	// Lire Scratchpad
 
-  // Convert the data to actual temperature
-  // because the result is a 16 bit signed integer, it should
-  // be stored to an "int16_t" type, which is always 16 bits
-  // even when compiled on a 32 bit processor.
-  int16_t raw = (data[1] << 8) | data[0];
-  if (type_s) {
-    raw = raw << 3; // 9 bit resolution default
-    if (data[7] == 0x10) {
-      // "count remain" gives full 12 bit resolution
-      raw = (raw & 0xFFF0) + 12 - data[6];
-    }
-  } else {
-    byte cfg = (data[4] & 0x60);
-    // at lower res, the low bits are undefined, so let's zero them
-    if (cfg == 0x00) raw = raw & ~7;  // 9 bit resolution, 93.75 ms
-    else if (cfg == 0x20) raw = raw & ~3; // 10 bit res, 187.5 ms
-    else if (cfg == 0x40) raw = raw & ~1; // 11 bit res, 375 ms
-    //// default is 12 bit resolution, 750 ms conversion time
-  }
-  celsius = (float)raw / 16.0;
-  Serial.print("  Temperature = ");
-  Serial.print(celsius);
-  Serial.print(" Celsius, ");
-  Serial.println("");
+	Serial.print("  Data = ");
+	Serial.print(data_temp.present, HEX);
+	Serial.print(" ");
+	for (i = 0; i < 9; i++) // 9 octets
+	{
+		data_temp.data[i] = Temperature.read();
+		Serial.print(data_temp.data[i], HEX);
+		Serial.print(" ");
+	}
+
+	Serial.print(" CRC=");
+	Serial.print(OneWire::crc8(data_temp.data, 8), HEX);
+	Serial.println();
+
+	// Convertir les données en température
+	// résultat 16 bits signées
+	int16_t raw = (data_temp.data[1] << 8) | data_temp.data[0];
+	if (data_temp.type_s)
+	{
+		raw = raw << 3;	// resolution 9 bits par défault
+		if (data_temp.data[7] == 0x10)
+		{
+			// "count remain" résolution 12 bit 
+			raw = (raw & 0xFFF0) + 12 - data_temp.data[6];
+		}
+	}
+	else
+	{
+		byte cfg = (data_temp.data[4] &0x60);
+		// en résolution basse les lsd sont non définie donc on les mets à zéro
+		if (cfg == 0x00) raw = raw &~7;	// 9 bits, 93.75 ms
+		else if (cfg == 0x20) raw = raw &~3;	// 10 bits, 187.5 ms
+		else if (cfg == 0x40) raw = raw &~1;	// 11 bits, 375 ms
+		//// defaut resolution 12 bit, 750 ms 
+	}
+
+	data_temp.celsius = (float) raw / 16.0;
+	Serial.print("  Temperature = ");
+	Serial.print(data_temp.celsius);
+	Serial.print(" Celsius, ");
+	Serial.println("");
+}
+
+/*
+ *SIM800L mode réception
+ */
+void ReceiveMode()
+{
+	Serial_Sim.println("AT");	//répond "OK"
+	updateSerial();
+	Serial_Sim.println("AT+CMGF=1");	// Configurer mode TEXT 
+	updateSerial();
+	Serial_Sim.println("AT+CNMI=2,2,0,0,0");	// paramètrage réception sms
+	updateSerial();
+}
+
+/*
+ *Envoie la chaine situé dans data_sim.sms_data au numéro data_sim.phone_number
+ */
+void Send_Data()
+{
+	Serial.println("Envoie des données sms ...");
+	Serial_Sim.print("AT+CMGF=1\r");	// mode SMS
+	delay(100);
+	Serial_Sim.print("AT+CMGS=\"" + data_sim.phone_number + "\"\r");
+	delay(500);
+	Serial_Sim.print(data_sim.sms_data);	// envoie le contenu de la variable 
+	delay(500);
+	Serial_Sim.print((char) 26);	// requis pour confirmer l'envoie
+	delay(500);
+	Serial_Sim.println();
+	Serial.println("Données envoyés.");
+	delay(500);
+
 }
 
 void updateSerial()
 {
-  char c;
-  delay(1000);
-  while (Serial.available()) 
-  {
-    Serial_Sim.write(Serial.read());//Forward what Serial received to Software Serial Port
-  }
-  while(Serial_Sim.available()) 
-  {
-    c = Serial_Sim.read();
-    Serial.write(c);//Forward what Software Serial received to Serial Port
-    if(c == '+')
-    {
-      for(int i =0; i < 128; i++)sms_text[i]=0;
-    }
-    sms_text[sms_text_index] = c;
-  }
+	while (Serial.available())
+	{
+		Serial_Sim.write(Serial.read());	//Renvoie ce qui est lu sur la liaison série GSM vers la console
+	}
+
+	while (Serial_Sim.available())
+	{
+		Serial.write(Serial_Sim.read());	//Renvoie ce qui est lu sur la liaison série PC vers le GSM (non utilisé)
+	}
 }
 
-void loop() {
-  updateSerial();
+void loop()
+{
+	String rx_full;	// concaténation des caractères lus par la liaison série
+	while (Serial_Sim.available() > 0)
+	{
+		data_sim.received = Serial_Sim.read();	//1 caractère
+		Serial.print(data_sim.received);
+		rx_full.concat(data_sim.received);	//on concatène le char au string
+		if (rx_full.indexOf("Temp") != -1)	// le message contient Temp -> on renvoie la température
+		{
+			read_Celsius();
+			data_sim.sms_data = "Temperature : " + String(data_temp.celsius) + " C";
+			Send_Data();
+			ReceiveMode();
+		}
+		else
+		{
+			if (rx_full.indexOf("Confort") != -1)	//  on positionne les optocoupleurs et en renvoie ok
+			{
+				set_Confort();
+				data_sim.mode = CONFORT;
+				EEPROM.write(EE_addr, data_sim.mode);
+				data_sim.sms_data = "OK Confort";
+				Send_Data();
+				ReceiveMode();
+			}
+			else
+			{
+				if (rx_full.indexOf("Arret") != -1)	// on positionne les optocoupleurs et en renvoie ok
+				{
+					set_Arret();
+					data_sim.mode = ARRET;
+					EEPROM.write(EE_addr, data_sim.mode);
+					data_sim.sms_data = "OK Arret";
+					Send_Data();
+					ReceiveMode();
+				}
+				else
+				{
+					if (rx_full.indexOf("Eco") != -1)	// on positionne les optocoupleurs et en renvoie ok
+					{
+						set_Eco();
+						data_sim.mode = ECO;
+						EEPROM.write(EE_addr, data_sim.mode);
+						data_sim.sms_data = "OK Eco";
+						Send_Data();
+						ReceiveMode();
+					}
+					else
+					{
+						if (rx_full.indexOf("Hors") != -1)	// on positionne les optocoupleurs et en renvoie ok
+						{
+							set_HorsGel();
+							data_sim.mode = HORS_GEL;
+							EEPROM.write(EE_addr, data_sim.mode);
+							data_sim.sms_data = "OK Hors gel";
+							Send_Data();
+							ReceiveMode();
+						}
+						else
+						{
+							if (rx_full.indexOf("Mode") != -1)	// on répond à la requete et on envoie le mode actuel
+							{
+								switch (data_sim.mode)
+								{
+									case ARRET:
+										data_sim.sms_data = "Mode arret";
+										break;
+									case CONFORT:
+										data_sim.sms_data = "Mode confort";
+										break;
+									case ECO:
+										data_sim.sms_data = "Mode eco";
+										break;
+									case HORS_GEL:
+										data_sim.sms_data = "Mode hors gel";
+										break;
+								}
+
+								Send_Data();
+								ReceiveMode();
+							}
+							else
+							{
+								if (rx_full.indexOf("Reset") != -1)	// on force le reset de l'arduino
+								{
+									wdt_enable(WDTO_15MS);
+									while (1) {}
+
+									Serial.println("Jamais atteint");
+								}
+								else
+								{
+									// pas reconnu on ne fait rien !!!  (si tel surtaxé envoie sms ???)
+								}
+							}
+						}
+					}
+				}
+			}
+		}
+	}
 }
